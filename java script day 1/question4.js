8ce4c630be4fd311b039a64df1f71780756d7cfd
SAKSHAMKUMAR-PROG <sakshamkumar79797@gmail.com>
2026-05-19 12:43:25 +0530

    functoin

--- java script day 1/question4.js
+++ java script day 1/question4.js
@@ -1,17 +1,10 @@
-function findPrimes(limit) {
-    const primes = [];
-    // Start from 2 because 1 is not a prime number
-    for (let num = 2; num <= limit; num++) {
-        let isPrime = true;
-        for (let i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) primes.push(num);
+for (let n = 2; n < 100; n++) {
+  let count = 0;
+  for (let i = 1; i <= n; i++)
+    if (n % i == 0) {
+      count++;
     }
-    return primes;
+  if (count == 2) {
+    console.log(n);
+  }
 }
-
-console.log(findPrimes(100));
