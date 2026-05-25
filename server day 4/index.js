1129f74bd6259ac515f697e0030c18a6b1276723
SAKSHAMKUMAR-PROG <sakshamkumar79797@gmail.com>
2026-05-25 13:53:28 +0530

    module

---
+++ server day 4/index.js
@@ -0,0 +1,8 @@
+const iseven = require("./module/iseven");
+iseven(23);
+
+const isodd = require("./module/isodd");
+isodd(24);
+
+const isprime = require("./module/isprime");
+isprime(25);
---
+++ server day 4/module/iseven.js
@@ -0,0 +1,8 @@
+function iseven(num) {
+  if (num % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+module.exports = iseven;
