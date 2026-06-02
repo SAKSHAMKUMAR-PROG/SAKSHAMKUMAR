75c63c5498aa2902e83f382680ade151e964ac44
SAKSHAMKUMAR-PROG <sakshamkumar79797@gmail.com>
2026-06-02 11:18:05 +0530

    add

--- day7javascriptserver/index.js
+++ day7javascriptserver/index.js
@@ -50,6 +50,36 @@ app.get("/news2", (req, res) => {
   }
 });
 
+app.get("/news2/:id", (req, res) => {
+  const catogory = req.params.category;
+  const limit = req.query.limit;
+
+  let result = news;
+
+  if (catogory) {
+    result = result.filter((i) => i.category === catogory);
+  }
+  if (limit) {
+    result = result.slice(0, number(limit));
+  }
+  res.send(result);
+});
+
+app.get("/news2", (req, res) => {
+  const limit = number(req.query.limit);
+  const from = req.query.from;
+  let result = news;
+
+  if (limit) {
+    if (from === "bottom") {
+      result = result.slice(-limit);
+    } else {
+      result = result.slice(0, limit);
+    }
+    res.send(result);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server running on localhost:3000");
 });
